b22d65b7bbd6ab66b9f561a04bcc17029795f2b6
Mahmoudemad2 <mahmoudemad9001@gmail.com>
2024-11-24 22:49:01 +0200

    my changes

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const loveName="Mahmoud";
 const input=document.getElementById("input").value;
 console.log(input);
 const divImg=document.getElementsByClassName("img-div")[0]
+const lov=document.getElementsByClassName("lov")[0]
 const Img=document.getElementsByClassName("img")[0]
 console.log(Img);
 console.log(divImg);
 body.addEventListener("click", function() {
 
   divImg.style.transform ="translateX(0)"
-  
+  lov.style.transform ="translateX(0)"
 });
